107cd05d4166b8d12dfb1ed831f8e1314ac08e21
julianlumets <84586104+julianlumets@users.noreply.github.com>
2023-03-28 10:12:42 +0200

    Initial commit

---
+++ SE_GO/src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module com.example.se_go {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.ikonli.javafx;
+
+    opens com.example.se_go to javafx.fxml;
+    exports com.example.se_go;
+}

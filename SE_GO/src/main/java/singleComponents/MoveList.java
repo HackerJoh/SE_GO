6c78c1b9af98caa7028a85c5f7e97a4be08436d6
OlegDontAsk <126416448+OlegDontAsk@users.noreply.github.com>
2023-06-15 13:55:48 +0200

    Deleted Unnecessary import

--- SE_GO/src/main/java/singleComponents/MoveList.java
+++ SE_GO/src/main/java/singleComponents/MoveList.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Paths;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;

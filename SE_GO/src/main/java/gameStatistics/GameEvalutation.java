5ddeac293aa71d155ed337ce1732bf406500b1d2
OlegDontAsk <126416448+OlegDontAsk@users.noreply.github.com>
2023-07-04 17:31:00 +0200

    Added classes for game statistics at the end

---
+++ SE_GO/src/main/java/gameStatistics/GameEvalutation.java
@@ -0,0 +1,25 @@
+package gameStatistics;
+
+import singleComponents.Move;
+import singleComponents.MoveList;
+import singleComponents.StoneColor;
+
+public class GameEvalutation {
+    private final StoneColor[][] board;
+    private final MoveList moveList;
+    private int blackPoints;
+    private int whitePoints;
+
+    public GameEvalutation(StoneColor[][] board, MoveList moveList){
+        this.board = board;
+        this.moveList = moveList;
+        this.blackPoints = moveList.getLastMove().getBlackPoints();
+        this.whitePoints = moveList.getLastMove().getWhitePoints();
+    }
+
+    public GameStatistics evaluateEndGameStatistics(){
+        return null;
+    }
+
+
+}
---
+++ SE_GO/src/main/java/gameStatistics/GameStatistics.java
@@ -0,0 +1,4 @@
+package gameStatistics;
+
+public record GameStatistics(int blackPoints, int whitePoints) {
+}
